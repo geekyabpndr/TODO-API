13ee77a36c64da21150245ac6973b97208c19397
geekyabpndr <48080680+geekyabpndr@users.noreply.github.com>
2019-03-13 11:21:48 +0530

    index.js deleted

--- index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    showtasks();
-    });
-    function showtasks(){
-        $.getJSON("http://api2.local.geekydev.com/read.php", function(data){
-            <table></table>;
-        });
-}
